a04b2ef451573313efffc824a08b5bb231f19431
ArthurYuCN <xxnjdlys@gmail.com>
2023-07-04 14:59:50 +0800

    Ant Design 动画.

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,14 @@
+<!--
+ * @Arthur: ArthurYuCN
+ * @Email: xxnjdlys@gmail.com
+ * @CreateTime: Do not edit
+ * @LastEditTime: 2023-07-04 14:51:43
+ * @LastEditors: ArthurYuCN xxnjdlys@gmail.com
+ * @FilePath: /myapp/src/pages/index/index.vue
+-->
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <view class="logo"></view>
   </view>
 </template>
 
@@ -14,7 +19,7 @@ export default {
       title: 'Hello',
     }
   },
-  onLoad() {},
+  onLoad() { },
   methods: {},
 }
 </script>
@@ -28,21 +33,43 @@ export default {
 }
 
 .logo {
-  height: 200rpx;
-  width: 200rpx;
   margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
+  width: 500rpx;
+  height: 100rpx;
+  position: relative;
+  color:
+    transparent;
+  background: url('https://imgservices-1252317822.image.myqcloud.com/image/012420220165011/c0e82c29.svg') center/contain no-repeat;
+  cursor: pointer;
 }
 
-.text-area {
-  display: flex;
-  justify-content: center;
+.logo::after {
+  content: '';
+  position: absolute;
+  width: 32rpx;
+  height: 32rpx;
+  background: url('https://imgservices-1252317822.image.myqcloud.com/image/012420220165415/b0005044.svg') 0 / cover no-repeat;
+  right: 113rpx;
+  top: -18rpx;
+  animation: random 1s steps(10) infinite;
+  /* 这里想指定到未来第7帧的位置，就可以延迟负的总运动时长的 7/10 */
+  animation-delay: -.7s;
+  /*动画暂停*/
+  animation-play-state: paused; 
+  /* 32 * 7 */
+  /* background-position: -224rpx;  */
 }
 
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+.logo:hover::after{
+     /*动画运行*/
+  animation-play-state: running;
 }
+
+@keyframes random {
+  to {
+    background-position: 100%;
+  }
+}
+
+
 </style>
